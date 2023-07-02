84e7b2dc7d51bacea78d73105326f7fa6b747a18
Ctfoee <hs.wks@mail.ru>
2023-07-02 17:22:31 +0300

    com

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminViewController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminViewController.java
@@ -1,27 +1,16 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import ru.kata.spring.boot_security.demo.service.UserService;
 
-import java.security.Principal;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminViewController {
 
-    private final UserService userService;
-
-    public AdminViewController(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping
-    public String adminPage(Model model, Principal principal) {
-        model.addAttribute("currentUser", userService.findByUsername(principal.getName()));
+    public String adminPage() {
         return "admin";
     }
 }
