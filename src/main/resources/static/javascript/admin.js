86806f918f652006fbef6c0779d2c75d0d8dc006
Ctfoee <hs.wks@mail.ru>
2023-07-06 16:15:21 +0300

    com

--- src/main/resources/static/javascript/admin.js
+++ src/main/resources/static/javascript/admin.js
@@ -130,6 +130,7 @@ $(document).ready(async function () {
             }
         })
         await updateUsersTable()
+        window.location = "/admin"
     })
 })
 
--- src/main/resources/static/javascript/util.js
+++ src/main/resources/static/javascript/util.js
@@ -16,11 +16,15 @@ export async function getAuthUsername() {
 export async function updateUserInfo() {
     let authUser = await getAuthUser()
     if (authUser != null) {
+        $("#usernameNavbar").text(authUser.username)
+        $("#userId").text(authUser.id)
         $("#username").text(authUser.username)
+        $("#userAge").text(authUser.age)
         let rolesText = " with roles: "
         for (let role of authUser.roles) {
             rolesText += `${role.role.substring(5)},  `
         }
-        $("#userRoles").text(rolesText.substring(0, rolesText.length - 3))
+        $("#userRolesNavbar").text(rolesText.substring(0, rolesText.length - 3))
+        $("#userRoles").text(rolesText.substring(12, rolesText.length - 3))
     }
 }
